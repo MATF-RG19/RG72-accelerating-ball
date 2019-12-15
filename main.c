b28364cedcd8243962e3eabd3ca40afce7b69a62
loshmi997 <mdjuric.djuric97@gmail.com>
2019-12-15 01:08:48 +0100

    Dodavanje loptice i njeno kretanje

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 #include<GL/glut.h>
 
+#define TIMER_ID 1
+#define TIMER_INTERVAL 20
+
 static void on_reshape(int width, int height);
 static void on_display(void);
+static void on_keyboard(unsigned char key, int x, int y);
+static void on_timer(int id);
 static void drawTrack();
+static void drawBall();
+
+int animation_ongoing;
+float animation_parameter;
+float move = 2.04;
+int acceleration = 1;
 
 int main(int argc,char** argv){
     
@@ -16,7 +27,8 @@ int main(int argc,char** argv){
     glutCreateWindow(argv[0]);
     glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
-    glClearColor(1,1,1,0);
+    glutKeyboardFunc(on_keyboard);
+    glClearColor(0,0,0,0);
     glEnable(GL_DEPTH_TEST);
     glutMainLoop();
     
@@ -29,31 +41,13 @@ static void on_display(void){
     
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(2,3,-5,
-                2,0,2,
-                0,10,0);
+    gluLookAt(2,3,animation_parameter-5,
+                2,0,animation_parameter+1,
+                0,1,0);
 
-    glBegin(GL_LINES);
-        glColor3f(1,1,0);
-        glVertex3f(0,0,0);
-        glVertex3f(10,0,0);
-        glVertex3f(0,0,0);
-        glVertex3f(-10,0,0);
-        
-        glColor3f(1,1,0);
-        glVertex3f(0,0,0);
-        glVertex3f(0,10,0);
-        glVertex3f(0,0,0);
-        glVertex3f(0,-10,0);
-
-        glColor3f(1,1,0);
-        glVertex3f(0,0,0);
-        glVertex3f(0,0,10);
-        glVertex3f(0,0,0);
-        glVertex3f(0,0,-10);
-    glEnd();
     
     drawTrack();
+    drawBall();
     
     glutSwapBuffers();
 }
@@ -69,13 +63,56 @@ static void on_reshape(int width, int height)
     gluPerspective(60, (float) width / height, 1, 1500);
 }
 
+static void on_keyboard(unsigned char key, int x, int y){
+    switch(key){
+        case 27:
+            exit(0);
+            break;
+        case 'a':
+        case 'A':
+            if(move<3.04){
+                move +=1;
+                glutPostRedisplay();
+                break;
+            }else break;
+        case 'd':
+        case 'D':
+            if(move>0.04){
+                move -=1;
+                glutPostRedisplay();
+                break;
+            }else break;
+            
+        case 'g':
+        case 'G':
+            if (!animation_ongoing) {
+                animation_ongoing = 1;
+                glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+            }
+        break;
+    }
+}
+
+static void on_timer(int id){
+    if (TIMER_ID != id)
+        return;
+    /*Podesava se parametar kretanja i ubrzanja loptice*/
+    animation_parameter += 0.0022*acceleration;
+    acceleration+=1;
+    glutPostRedisplay();
+
+    if (animation_ongoing) {
+        glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+    }
+}
+
 void drawTrack()
 {
     /*Iscrtava se staza*/
     glPushMatrix();
         glColor3f(0.3, 0.3, 0.3);
-        glTranslatef(2.5,0.125,5);
-        glScalef(5,0.25,10);
+        glTranslatef(2.06,0.125,5);
+        glScalef(5,0.25,100000);
         glutSolidCube(1);
     glPopMatrix();
     
@@ -83,29 +120,37 @@ void drawTrack()
     
     glPushMatrix();
         glColor3f(1,1,0);
-        glTranslatef(1,0.2,5);
-        glScalef(0.1,0.25,10);
+        glTranslatef(0.54,0.2,5);
+        glScalef(0.1,0.25,100000);
         glutSolidCube(1);
     glPopMatrix();
       
     glPushMatrix();
         glColor3f(1,1,0);
-        glTranslatef(3,0.2,5);
-        glScalef(0.1,0.25,10);
+        glTranslatef(2.54,0.2,5);
+        glScalef(0.1,0.25,100000);
         glutSolidCube(1);
     glPopMatrix();
     
     glPushMatrix();
         glColor3f(1,1,0);
-        glTranslatef(2,0.2,5);
-        glScalef(0.1,0.25,10);
+        glTranslatef(1.54,0.2,5);
+        glScalef(0.1,0.25,100000);
         glutSolidCube(1);
     glPopMatrix();
     
     glPushMatrix();
         glColor3f(1,1,0);
-        glTranslatef(4,0.2,5);
-        glScalef(0.1,0.25,10);
+        glTranslatef(3.54,0.2,5);
+        glScalef(0.1,0.25,100000);
         glutSolidCube(1);
+    glPopMatrix();
+}
+
+void drawBall(){
+    glPushMatrix();
+        glColor3f(0,0,1);
+        glTranslatef(move,0.5,animation_parameter);
+        glutSolidSphere(0.25,100,100);
     glPopMatrix();
 }
